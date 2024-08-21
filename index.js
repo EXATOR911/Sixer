c3a1dce71bf9b9b256cb5ea86bac5e158792e9dd
Eleanor <matthewbates0727@gmail.com>
2024-08-21 12:19:57 -0400

    LET'S GET THIS PARTY STARTED!!

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-var blink_speed = 1000; // every 1000 == 1 second, adjust to suit
+window.onload = function () {
+  var SixersSong = document.getElementById("ForYouSixer");
+  SixersSong.play();
+};
+
+document.getElementById("Tri").addEventListener("click", function () {
+  {
+    alert("I'll be seeing you Pines family! You can't kill an idea!!");
+  }
+});
+
+var blink_speed = 1000;
 var t = setInterval(function () {
   var ele = document.getElementById("Tri");
   ele.style.visibility = ele.style.visibility == "hidden" ? "" : "hidden";
